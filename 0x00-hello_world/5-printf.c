83497501bcdadc8a5820113f246515890b22c3db
qoudri4re <abdulqodrioladimeji@gmail.com>
2022-03-10 01:23:33 -0800

    adding 5-printf.c file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints a sentence using the printf function
+ *
+ * Return: returns 0 after printing
+*/
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art");
+	return (0);
+}
