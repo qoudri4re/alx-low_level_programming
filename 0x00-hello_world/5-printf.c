826895b90fc6359be9e3078f464abaf7cf67806f
qoudri4re <63318729+qoudri4re@users.noreply.github.com>
2022-03-10 10:32:56 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - prints a sentence using the printf function
+ * main - prints a sentence with the printf function
  *
  * Return: returns 0 after printing
-*/
-
+ */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
