9adf3c90a0203fb9b5b8f3f473056a14cac13cc9
qoudri4re <63318729+qoudri4re@users.noreply.github.com>
2022-03-11 15:23:48 +0100

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,1 +1,17 @@
-i
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int num;
+
+for (num = 48; num <= 57; num++)
+{
+putchar(num);
+}
+putchar('\n');
+return (0);
+}
