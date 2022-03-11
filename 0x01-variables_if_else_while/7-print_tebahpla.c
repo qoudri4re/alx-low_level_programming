ca348319116c8f56d90ea6e89c059224c7187124
qoudri4re <63318729+qoudri4re@users.noreply.github.com>
2022-03-11 15:28:45 +0100

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,1 +1,16 @@
-jdj
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char alpha;
+for (alpha = 'z'; alpha <= 'a'; alpha++)
+{
+putchar(alpha);
+}
+putchar('\n');
+return (0);
+}
