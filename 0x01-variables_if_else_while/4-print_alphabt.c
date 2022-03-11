058b12acbfec81ce41644ca0c97ae0b2ff4a7b09
qoudri4re <63318729+qoudri4re@users.noreply.github.com>
2022-03-11 11:09:49 +0100

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,19 @@
-iiiii
+#include <stdio.h>
+/**
+ * main - where programs are executed
+ *
+ * Return: returns 0
+ */
+
+int main(void)
+{
+  char alpha;
+  for (alpha='a'; alpha <= 'z'; alpha++)
+  {
+    if (alpha != 'e' || alpha != 'q')
+    {
+      putchar(alpha);
+      putchar('\n');
+    }
+  }
+}
