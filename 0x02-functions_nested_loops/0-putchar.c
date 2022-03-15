741f66bfe5b508b500dceeac10694e34b3325196
qoudri4re <63318729+qoudri4re@users.noreply.github.com>
2022-03-15 10:56:19 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,22 @@
-i
+#include <stdio.h>
+#include "greet.h"
+
+/**
+ * main - Prints Holberton
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void){
+  _putchar('_');
+  _putchar('p');
+  _putchar('u');
+  _putchar('t');
+  _putchar('c');
+  _putchar('h');
+  _putchar('a');
+  _putchar('r');
+  _putchar('\n');
+  return (0);
+}  
+  
+  
