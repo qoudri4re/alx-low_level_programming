7a64369932b0620ea6d201e7db944c7a635b8452
qoudri4re <63318729+qoudri4re@users.noreply.github.com>
2022-03-15 11:20:52 +0100

    Update and rename _print_alphabet.c to print_alphabet.c

---
+++ 0x02-functions_nested_loops/print_alphabet.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+* print_alphabet - prints the alphabets
+*
+* Return: returns 0
+*/
+
+void print_alphabet(void)
+{
+  char alpha;
+  for (alpha = 'a'; alpha <= 'z'; alpha++)
+  {
+    putchar(alpha);
+  }
+  putchar('\n');
+}
