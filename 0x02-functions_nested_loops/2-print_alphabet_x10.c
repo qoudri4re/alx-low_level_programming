a464a64a3a0cc92a8acf21e1923d4a86140b1aed
qoudri4re <qoudri4re@gmail.com>
2022-03-15 04:01:43 -0700

    fixed betty styling error

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,17 +9,14 @@
 
 void print_alphabet_x10(void)
 {
-  char alpha;
-  int i;
-  for (i = 1; i <= 10; i++)
-  {
-    
-  for (alpha = 'a'; alpha <= 'z'; alpha++)
-  {
-    _putchar(alpha);
-  }
-  _putchar('\n');
-    
-    
-  }
+char alpha;
+int i;
+for (i = 1; i <= 10; i++)
+{
+for (alpha = 'a'; alpha <= 'z'; alpha++)
+{
+_putchar (alpha);
+}
+_putchar ('\n');
+}
 }
--- 0x02-functions_nested_loops/test.c
+++ 0x02-functions_nested_loops/test.c
@@ -16,7 +16,7 @@ int main(void)
 
 {
 
-	    print_alphabet();
+	    print_alphabet_x10();
 
 	        return (0);
 
