b4bbe9529f28df026ed0b124413b004bcfe9d3ec
qoudri4re <63318729+qoudri4re@users.noreply.github.com>
2022-03-15 15:34:19 +0100

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-* main - Lists all the natural numbers below 
+* main - Lists all the natural numbers below
 *1024 (excluded)nthat are multiples of 3 or 5.
 *
 * Return: Always 0.
