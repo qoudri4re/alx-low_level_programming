da439cf2f75d676a3c586814dda3589bf18f2c93
qoudri4re <qoudri4re@gmail.com>
2022-03-15 04:32:05 -0700

    fixed betty styling errors

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,17 +6,18 @@
 /**
 * _islower - checks for a lowercase character
 *
+* @c: the character to be checked
+*
 * Return: returns 1 if the character is lower case and 0 otherwise
 */
 int _islower(int c)
 {
-  if (islower(c) != 0)
-  {
-    return (1);
-  } 
-  else 
-  {
-    return (0);
-  }
+if (islower(c) != 0)
+{
+return (1);
 }
-  
+else
+{
+return (0);
+}
+}
