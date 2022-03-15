55826d96b3104627ae16d0b402ed5a64f794f84a
qoudri4re <qoudri4re@gmail.com>
2022-03-15 04:57:29 -0700

    adding 5-sign.c file and updated main.h file

--- 0x02-functions_nested_loops/test.c
+++ 0x02-functions_nested_loops/test.c
@@ -13,28 +13,30 @@
  *     */
 
 int main(void)
-
 {
-
-	    int r;
-
+	 int r;
 
 
-	        r = _islower('H');
 
-		    _putchar(r + '0');
+	     r = _isalpha('H');
 
-		        r = _islower('o');
+	         _putchar(r + '0');
 
-			    _putchar(r + '0');
+		     r = _isalpha('o');
 
-			        r = _islower(108);
+		         _putchar(r + '0');
 
-				    _putchar(r + '0');
+			     r = _isalpha(108);
 
-				        _putchar('\n');
+			         _putchar(r + '0');
 
-					    return (0);
+				     r = _isalpha(';');
+
+				         _putchar(r + '0');
+
+					     _putchar('\n');
+
+					        
 
 	        return (0);
 
