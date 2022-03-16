20ba1e51da148e9543475b743b512aeccb876b39
qoudri4re <63318729+qoudri4re@users.noreply.github.com>
2022-03-16 22:21:35 +0100

    Update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,6 +2,6 @@
 #define MAINN_H
 
 int largest_number(int, int, int);
-void positive_or_negative(int i);
-void print_remaining_days(int, int, int)
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year)
 #endif
