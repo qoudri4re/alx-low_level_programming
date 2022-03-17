3751707e4c46612e7fea6dfeed34169ec51c5057
qoudri4re <63318729+qoudri4re@users.noreply.github.com>
2022-03-17 10:46:49 +0100

    Update 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,1 +1,20 @@
-ii
+#include <ctype.h>
+#include "main.h"
+
+/**
+* _isdigit - checks if a variable is a digit
+* @c: the variable to be checked
+* Return: returns 1 if the variable is uppercase and zero otherwise
+*/
+
+int _isdigit(int c)
+{
+if (isdigit(ch))
+{
+return (1);
+}
+else 
+{
+return (0);
+}
+}
