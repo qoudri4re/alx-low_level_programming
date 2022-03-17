076a4e09db129153904ebd627154ec24008ec227
qoudri4re <63318729+qoudri4re@users.noreply.github.com>
2022-03-17 10:31:25 +0100

    Update 0-main.c

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -1,2 +1,17 @@
-i
-t
+#include "main.h"
+#include <stdio.h>
+/**
+* main - check the code.
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+char c;
+
+c = 'A';
+printf("%c: %d\n", c, _isupper(c));
+c = 'a';
+printf("%c: %d\n", c, _isupper(c));
+return (0);
+}
