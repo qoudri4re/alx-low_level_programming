3baf117565611c41bb2b50bd37b1c3c73287940d
qoudri4re <63318729+qoudri4re@users.noreply.github.com>
2022-03-17 12:51:38 +0100

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,10 +14,14 @@ int main(void)
 int i;
 for (i = 1; i <= 100; 1++)
 {
-if (i % 3 == 0) printf("Fizz");
-else if (i % 5 == 0) printf("Buzz");
-else if (i % 3 == 0 && i % 5 == 0) printf("FizzBuzz");
-else printf("%d", i);
+if (i % 3 == 0)
+  printf("Fizz");
+else if (i % 5 == 0)
+  printf("Buzz");
+else if (i % 3 == 0 && i % 5 == 0)
+  printf("FizzBuzz");
+else
+  printf("%d", i);
 }
 return (0);
 }
