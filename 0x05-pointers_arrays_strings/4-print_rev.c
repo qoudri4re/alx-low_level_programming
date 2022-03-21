08907209dd2b25d009698b41b9539c91f1508a01
qoudri4re <63318729+qoudri4re@users.noreply.github.com>
2022-03-21 08:18:16 +0100

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,7 +12,7 @@ void print_rev(char *s)
 int left_index = 0;
 int right_index = strlen(s) - 1;
 int i;
-for (i = left_index; i < right_index; i++)
+for (i = right_index; i < left_index; i--)
 {
 _putchar(s[i]);
 }
