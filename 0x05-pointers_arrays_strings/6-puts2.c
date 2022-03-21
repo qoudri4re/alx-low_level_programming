b6c922da7900666d0f3ef1e8afb65365ca34f95a
qoudri4re <63318729+qoudri4re@users.noreply.github.com>
2022-03-21 10:00:45 +0100

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * puts2 - prints every other character of a  string
+ * puts2 - prints every other character of a  string to stdout
  * @str: string to print
  * Return: Always 0
  */
