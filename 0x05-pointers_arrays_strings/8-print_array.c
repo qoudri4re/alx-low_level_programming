ca549181b23c179388aa3f87a6f768f7fb4c0bc5
qoudri4re <63318729+qoudri4re@users.noreply.github.com>
2022-03-21 09:53:33 +0100

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,1 +1,19 @@
-iiii
+#include "main.h"
+
+/**
+* print_rev - prints a string, in reverse, followed by a new line.
+*@s: string to be printed in reverse
+* Return: returns 0
+*/
+void print_array(int *a, int n){
+int i = 0;
+while (i < n)
+{
+printf("%d", a[i]);
+if (i < (n - 1)
+{
+printf(", ");
+}
+i++;
+}
+}
