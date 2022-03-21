331dca761acbae93e6f04fc6a73cf086af098c4a
qoudri4re <63318729+qoudri4re@users.noreply.github.com>
2022-03-21 07:51:02 +0100

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,1 +1,14 @@
-ii
+#include <string.h>
+#include "main.h"
+
+/**
+* _puts - prints a string, followed by a new
+* line, to stdout.
+*@str: string to be printed
+* Return: returns 0
+*/
+void _puts(char *str)
+{
+puts(str);
+puts("\n");
+}
