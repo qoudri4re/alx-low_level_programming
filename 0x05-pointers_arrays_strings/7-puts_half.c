7261e4219aaaaa637807e1e7203e9ff1a289929e
qoudri4re <63318729+qoudri4re@users.noreply.github.com>
2022-03-21 09:39:45 +0100

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -20,9 +20,9 @@ if (length % 2 == 0)
 }
 else
 {
-for (i = half; i <= length - 1; i++)
-_putchar(str[i]);
+  for (i = half; i <= length - 1; i++)
+  _putchar(str[i]);
+  }
 }
-}
-_putchar("\n");
+_putchar('\n');
 }
