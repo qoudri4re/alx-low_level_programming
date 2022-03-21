338a6ea984ffc7b64e6c0d0ac8a77f5bd4da87f6
qoudri4re <63318729+qoudri4re@users.noreply.github.com>
2022-03-21 08:46:45 +0100

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,13 +9,13 @@
 void print_rev(char *s)
 {
 int length  = sizeof(s)/sizeof(s);
-char reversed_string[length];
+char reversed_string;
 int right_index = length - 1;
 int left_index = 0;
 int i;
 for (i = right_index; i >= left_index; i--)
 {
-strcat(reversed_string, s[i]);
+reversed_string += s[i];
 }
 return(reversed_string);
 }
