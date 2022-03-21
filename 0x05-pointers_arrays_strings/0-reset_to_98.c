84fc29e57b710425b324f98cbbc78adc5bf52b7c
qoudri4re <63318729+qoudri4re@users.noreply.github.com>
2022-03-21 07:05:18 +0100

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,13 @@
 #include "main.h"
 
 /**
-* print_to_98 - prints all natural numbers from n to 98
-*@n: number
+* reset_to_98 - a function that takes a pointer to an 
+*int as parameter and updates the value it 
+*points to to 98.
+*@n: the pointer
 * Return: returns 0
 */
-void reset_to_98(int *n){
-  *n = 98;
+void reset_to_98(int *n)
+{
+*n = 98;
 }
