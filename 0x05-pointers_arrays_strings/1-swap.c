940d2f03d67e8dcce3fa8f003a158ac2e1fc7b66
qoudri4re <63318729+qoudri4re@users.noreply.github.com>
2022-03-21 07:11:52 +0100

    Update 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,1 +1,15 @@
-iii
+#include "main.h"
+
+/**
+* void swap_int - prints all natural numbers from n to 98
+*@a: pointer to the first number
+*@b: pointer to the second number
+* Return: returns 0
+*/
+void swap_int(int *a, int *b)
+{
+int temp;
+temp = *a;
+*a = *b;
+*b = temp;
+}
