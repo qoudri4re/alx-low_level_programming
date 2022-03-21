3a35c140c4da57b070ba2229a798381dadeb3f12
qoudri4re <63318729+qoudri4re@users.noreply.github.com>
2022-03-21 09:13:27 +0100

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,1 +1,19 @@
-iii
+#include <stdio.h>
+#include "main.h"
+/**
+* puts2 - prints every other character of a  string
+* @str: string to print
+* Return: Always 0
+*/
+void puts2(char *str)
+{
+int i = 0;
+
+while (*(str + i))
+{
+putchar(*(str + i));
+i = i + 2;
+}
+putchar('\n');
+}
+© 2022 G
