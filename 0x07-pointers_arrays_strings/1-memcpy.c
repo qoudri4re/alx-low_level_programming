7f1ab23bd41dacff4fa1d0d2da593821979b15a8
qoudri4re <63318729+qoudri4re@users.noreply.github.com>
2022-03-28 10:40:14 +0100

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,20 +1,19 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * _memcpy - irontnoux2
- * @dest: pointer to char params
- * @src: pointer to char params
- * @n: size
- * Return: *dest
- */
+* _memcpy - irontnoux2
+* @dest: pointer to char params
+* @src: pointer to char params
+* @n: size
+* Return: *dest
+*/
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
-
-	for (i = 0; i < n; i++)
-	{
-		dest[i] = src[i];
-	}
-	return (dest);
+unsigned int i;
+for ( i = 0; i < n; i++)
+{
+dest[i] = src[i];
+}
+return dest;
 }
