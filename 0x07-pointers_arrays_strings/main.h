3f43850f308edc14055e4df677f5a26e3917d6fe
qoudri4re <63318729+qoudri4re@users.noreply.github.com>
2022-03-28 10:28:44 +0100

    Update and rename holberton.h to main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef FILE_HOLBERTON
-#define FILE_HOLBERTON
+#ifndef MAIN_H
+#define MAIN_H
 
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
