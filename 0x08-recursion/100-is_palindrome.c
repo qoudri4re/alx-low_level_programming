d464d3f62f0f00d6fd5aee7c294d70b8992e4a76
qoudri4re <63318729+qoudri4re@users.noreply.github.com>
2022-03-30 12:36:46 +0100

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,50 +1,47 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
- * _strlen_recursion - size
- * @s: pointer to string params
- * Return: recursion
- */
-
+* _strlen_recursion - size
+* @s: pointer to string params
+* Return: recursion
+*/
 int _strlen_recursion(char *s)
 {
-	if (!*s)
-	{
-		return (0);
-	}
-	return (1 + _strlen_recursion(++s));
+if (!*s)
+{
+return (0);
 }
-
+return (1 + _strlen_recursion(++s));
+}
 /**
- * p1 - palindrome
- * @s: pointer to string
- * @l: position
- * Return: boolena
- */
+* p1 - palindrome
+* @s: pointer to string
+* @l: position
+* Return: boolena
+*/
 
 int p1(char *s, int l)
 {
-	if (l < 1)
-	{
-		return (1);
-	}
+if (l < 1)
+{
+return (1);
+}
 
-	if (*s == *(s + l))
-	{
-		return (p1(s + 1, l - 2));
-	}
-	return (0);
+if (*s == *(s + l))
+{
+return (p1(s + 1, l - 2));
+}
+return (0);
 }
 
 /**
- * is_palindrome - palindrome
- * @s: pointer to string
- * Return: recursion
- */
+* is_palindrome - palindrome
+* @s: pointer to string
+* Return: recursion
+*/
 
 int is_palindrome(char *s)
 {
-	int len = _strlen_recursion(s);
+int len = _strlen_recursion(s);
 
-	return (p1(s, len - 1));
+return (p1(s, len - 1));
 }
