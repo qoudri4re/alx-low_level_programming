1c0a0433727d77f936d41e2789bab175c9f6dee0
qoudri4re <63318729+qoudri4re@users.noreply.github.com>
2022-03-30 11:37:13 +0100

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,1 +1,14 @@
-iii
+#include "main.h"
+/**
+* _print_rev_recursion - reverse
+* @s: pointer to string params
+* 
+*/
+void _print_rev_recursion(char *s)
+{
+if (*s)
+{
+_print_rev_recursion(s + 1);
+_putchar(*s);
+}
+}
