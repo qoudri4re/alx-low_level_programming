07b23a6b0ef047e45b304eec6bed0fbcf43be5a7
qoudri4re <63318729+qoudri4re@users.noreply.github.com>
2022-03-30 12:08:14 +0100

    Update 4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-*_pow_recursion - a function that returns the value of x 
+*_pow_recursion - a function that returns the value of x
 *raised to the power of y
 * @x: exponent
 * @y: power
