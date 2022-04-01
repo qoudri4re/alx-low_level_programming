07e42979f5dd42a65778c52ba0f6c8c879a3cf83
qoudri4re <63318729+qoudri4re@users.noreply.github.com>
2022-04-01 14:46:42 +0100

    Update 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -6,12 +6,13 @@
 * @argv: offset value
 * Return: 0
 */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-  int i;
-  for (i = 0; i < argc; i++)
-  {
-    printf("%s\n", argv[i]);
-  }
-  return (0);
+int i;
+
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+return (0);
 }
