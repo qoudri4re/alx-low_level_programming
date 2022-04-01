65a958d67318769419a4274b984e37d4cd3eba28
qoudri4re <63318729+qoudri4re@users.noreply.github.com>
2022-04-01 15:10:59 +0100

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,26 +11,26 @@
 */
 int main(int argc, char *argv[])
 {
-        unsigned int i, sum, num;
-        sum = 0;                                                                                                                        
-        if (argc < 3)
-        {
-                printf("%d\n", 0);
-                return (0);
-        }
-        while (argc-- && argc > 0)
-                  {
-                for (i = 0; argv[argc][i] != '\0'; i++)
-                {
-                        if (!(isdigit(argv[argc][i])))
-                        {
-                                printf("Error\n");
-                                return (1);
-                        }
-                }
-                num = atoi(argv[argc]);
-                sum += num;
-        }
-        printf("%d\n", sum);
-        return (sum);
+unsigned int i, sum, num;
+sum = 0;
+if (argc < 3)
+{
+printf("%d\n", 0);
+return (0);
+}
+while (argc-- && argc > 0)
+{
+for (i = 0; argv[argc][i] != '\0'; i++)
+{
+if (!(isdigit(argv[argc][i])))
+{
+printf("Error\n");
+return (1);
+}
+}
+num = atoi(argv[argc]);
+sum += num;
+}
+printf("%d\n", sum);
+return (sum);
 }
