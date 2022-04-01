31e8a504fdcb3000da4f311fcbe541c5a506cb96
qoudri4re <63318729+qoudri4re@users.noreply.github.com>
2022-04-01 09:42:21 +0100

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,1 +1,13 @@
-ii
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* main - prints the argument count
+* @argc: offset count
+* @argv: offset value
+* Return: 0
+*/
+int main(int argc, char* argv[])
+{
+  printf("%d\n", argc);
+  return (0);
+}
