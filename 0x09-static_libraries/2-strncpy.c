80da38acab846fed77a1f37a1978dab7bae7a973
qoudri4re <63318729+qoudri4re@users.noreply.github.com>
2022-04-02 13:16:12 +0100

    Update 2-strncpy.c

--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -1,1 +1,16 @@
-ii
+#include "main.h"
+
+/**
+ *@dest: pointer
+ *@src: pointer
+ *@n: parameter
+ *Return:dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	(void)dest;
+	(void)src;
+	(void)n;
+	return (dest);
+}
