308d67b53c5772c3f4aebd884cf4b14df688855d
qoudri4re <63318729+qoudri4re@users.noreply.github.com>
2022-04-04 10:27:12 +0100

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -6,22 +6,18 @@
 * @c: constant
 * Return: char pointer
 */
-char *create_array(unsigned int size, char c)
-{
-  int *p;
-  unsigned int i;
-  if (size == 0)
-  {
-    return (0);
-  }
-  *p = (int*)malloc(size * sizeof(char));
-  if (p == '\0')
-  {
-    return (0);
-  }
-  for (i = 0; i < size; i++)
-  {
-    p[i] = c;
-  }
-  return p;
-}
+char *create_array(unsigned int size, char c)                                                                                           
+{                                                                                                                                       
+        char *p;                                                                                                                        
+        unsigned int i;                                                                                                                 
+                                                                                                                                        
+        if (size == 0)                                                                                                                  
+        return (0);                                                                                                                     
+        p = malloc((size) * sizeof(char));                                                                                              
+        if (p == '\0')                                                                                                                  
+        return (0);                                                                                                                     
+        for (i = 0; i < size; i++)                                                                                                      
+        {                                                                                                                               
+                p[i] = c;                                                                                                               
+        }                                                                                                                               
+return (p);                                                                                                                             
