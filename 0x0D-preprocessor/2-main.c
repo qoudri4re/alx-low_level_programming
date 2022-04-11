bafb85aad3de186373ee103b4b7b65c03f8f8b60
qoudri4re <63318729+qoudri4re@users.noreply.github.com>
2022-04-11 11:45:26 +0100

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,1 +1,13 @@
-ii
+#include <stdio.h>
+/**
+* main - prints  the name of the file
+* it was compiled from, followed by a new line.
+*
+* Return: returns 0 after printing
+*/
+
+int main(void)
+{
+  printf("%s", __FILE__);
+  return (0);
+}
