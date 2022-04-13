4d010a9f36d47c5f008c29d9ca4c09935f9b8957
qoudri4re <63318729+qoudri4re@users.noreply.github.com>
2022-04-13 11:06:18 +0100

    Update function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,7 @@
 #ifndef FILE_F_POINTRS
 #define FILE_F_POINTRS
 
+#include <stddef.h>
 void print_name(char *name, void (*f)(char *));
 int _putchar(char c);
 void array_iterator(int *array, size_t size, void (*action)(int));
