a40c46cea43be6c5be62d4b95df3fa7763334944
qoudri4re <63318729+qoudri4re@users.noreply.github.com>
2022-05-03 11:13:15 +0100

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,5 +12,5 @@
 int append_text_to_file(const char *filename, char *text_content);
 int create_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
-
+void print_elf_header(void *h)
 #endif
